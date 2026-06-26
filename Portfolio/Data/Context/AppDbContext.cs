1b32d5b4cceb5f00afe9964a4bb57f2c2bca5cbf
Cihan Şahin <cihaansaahin@gmail.com>
2026-06-26 16:14:49 +0300

    Add project files.

---
+++ Portfolio/Data/Context/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Data.Entities;
+
+namespace Portfolio.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=PortfolioDb;Integrated Security=true;trustservercertificate=true");
+
+        }
+        //Pluralize
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<TechStack> TechStacks { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<ProjectTechStack> ProjectTechStacks { get; set; }
+
+
+
+    }
+}
---
+++ Portfolio/Data/Entities/ProjectTechStack.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Data.Entities
+{
+    public class ProjectTechStack
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        // Navigation property to the Project entity
+        public Project Project { get; set; }
+        public int TechStackId   { get; set; }
+        // Navigation property to the TechStack entity
+        public TechStack TechStack { get; set; }
+    }
+}
